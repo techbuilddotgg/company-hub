2cbd1493b3a9d6296402e2c171f8362d8b02cf7f
perkzen <perko.domen@gmail.com>
2023-05-07 19:42:57 +0200

    fix: calendar page

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -4,8 +4,8 @@ import { useUser } from '@clerk/nextjs';
 const Calendar = () => {
   const user = useUser();
 
-  if (!user) {
-    new Error('Do not have access to this page');
+  if (!user.isSignedIn) {
+    throw new Error('Do not have access to this page');
   }
 
   return <div>Calendar</div>;
