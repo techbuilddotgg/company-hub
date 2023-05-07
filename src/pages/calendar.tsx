464dbcd640eed7cde02e5880c4c192316ba2d756
perkzen <perko.domen@gmail.com>
2023-05-07 19:40:51 +0200

    fix: calendar page

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useUser } from '@clerk/nextjs';
 
 const Calendar = () => {
-  throw new Error('Not implemented');
+  const user = useUser();
+
+  if (!user) {
+    new Error('Do not have access to this page');
+  }
 
   return <div>Calendar</div>;
 };
