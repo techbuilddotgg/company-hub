293a90462ca2e803d6d6bcd695afd66460a9ea9c
perkzen <perko.domen@gmail.com>
2023-05-08 16:29:56 +0200

    chore: remove uninsured prop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import '../styles/globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
-import type { AppType } from 'next/app';
-import { trpc } from '@utils/trpc';
-import Layout from '@components/layout';
+import "../styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppType } from "next/app";
+import { trpc } from "@utils/trpc";
+import Layout from "@components/layout";
 
-const MyApp: AppType = ({ Component, pageProps, router }) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Layout>
