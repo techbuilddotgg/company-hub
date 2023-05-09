d93e4826dc54e3aaea32bab9931e66cfa90107c2
jernej10 <jernej.ofic@gmail.com>
2023-05-09 13:43:28 +0200

    Feat: trello components

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import Board from "@components/trello/Board";
 
 const Projects = () => {
-  return <div>Projects</div>;
+
+  return(
+    <div className='ml-10'>
+      <h1 className='font-bold text-2xl my-4'>Project 1</h1>
+      <Board />
+    </div>
+  );
 };
 
 export default Projects;
