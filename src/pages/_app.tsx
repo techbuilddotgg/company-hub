a6313ad42dfcb5dee2f65c46122dd65fc946caf3
Matevz <semprimoznik.matevz@gmail.com>
2023-05-09 09:18:18 +0200

    remove navbar from signin and signup page, add sign in url in sign up component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,18 @@ import { ClerkProvider } from '@clerk/nextjs';
 import type { AppType } from 'next/app';
 import { trpc } from '@utils/trpc';
 import Layout from '@components/layout';
+import { AppRoute } from "@constants/app-routes";
 
 const MyApp: AppType = ({ Component, pageProps, router }) => {
+  const useLayout = ![AppRoute.SIGN_IN, AppRoute.SIGN_UP].includes(router.pathname as AppRoute)
+
   return (
     <ClerkProvider {...pageProps}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      {useLayout ? (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      ) : <Component {...pageProps} />}
     </ClerkProvider>
   );
 };
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
+import { AppRoute } from "@constants/app-routes";
 
 const SignUpPage = () => (
   <div className='flex items-center justify-center h-5/6'>
-    <SignUp/>
+    <SignUp signInUrl={AppRoute.SIGN_IN}/>
   </div>
 );
 
