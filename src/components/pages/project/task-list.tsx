2aec2d4d5eb57516ee0cee0ce0ce14fbbc1d51f3
jernej10 <jernej.ofic@gmail.com>
2023-05-14 19:08:41 +0200

    Feat: redesign trello board

--- src/components/pages/project/task-list.tsx
+++ src/components/pages/project/task-list.tsx
@@ -15,12 +15,7 @@ const TaskList = ({ data, id }: TicketListProps) => {
         <div
           {...provided.droppableProps}
           ref={provided.innerRef}
-          style={{
-            background: snapshot.isDraggingOver ? 'lightblue' : 'lightgrey',
-            padding: 4,
-            width: 250,
-            minHeight: 500,
-          }}
+          className=" bg-gray-100 p-4"
         >
           {data.map((task, index) => (
             <Task key={index} data={task} index={index} />
