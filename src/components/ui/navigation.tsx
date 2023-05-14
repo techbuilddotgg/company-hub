e006c5130b45f663a2d413b313d7f503f01302a2
perkzen <perko.domen@gmail.com>
2023-05-14 15:34:41 +0200

    feat: prettier nav

--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { AppRoute } from '@constants/app-routes';
 import Link from 'next/link';
-import { SignInButton, SignedOut, SignedIn } from '@clerk/nextjs';
-import { SignOutButton } from '@clerk/clerk-react';
+import { SignedIn, UserButton, useUser } from '@clerk/nextjs';
+import { useRouter } from 'next/router';
+import { cn } from '@utils/classNames';
 
 const links: { name: string; href: AppRoute }[] = [
   { name: 'Overview', href: AppRoute.HOME },
@@ -12,28 +13,51 @@ const links: { name: string; href: AppRoute }[] = [
 ];
 
 export const Navigation = () => {
+  const router = useRouter();
+
+  const user = useUser();
+
+  const isActive = (href: AppRoute) => router.pathname === href;
+
   return (
     <div className=" flex h-16 min-h-screen flex-col items-center border-r px-6">
-      <nav className=" mt-10 flex grow flex-col items-center gap-4">
-        {links.map(({ name, href }) => (
-          <Link href={href} key={name}>
-            <a
-              className={
-                'text-sm font-medium transition-colors hover:text-primary'
-              }
+      <h1 className={'my-4'}>
+        Tech<span className={'text-blue-600'}>Build</span>
+      </h1>
+      <nav className=" mt-4 flex grow flex-col gap-4">
+        <ul className={'flex grow flex-col gap-4'}>
+          {links.map(({ name, href }) => (
+            <li
+              key={name}
+              className={cn(
+                'rounded p-2 text-gray-500',
+                isActive(href) && 'bg-blue-100 text-blue-600',
+                !isActive(href) &&
+                  'text-gray-500 hover:bg-gray-100 hover:text-black',
+              )}
             >
-              {name}
-            </a>
-          </Link>
-        ))}
+              <Link href={href}>
+                <a
+                  className={
+                    'rounded-md p-2 text-sm font-medium transition-colors'
+                  }
+                >
+                  {name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </nav>
-      <div className={'mb-6'}>
+      <div className={'mb-6 flex w-full flex-row'}>
         <SignedIn>
-          <SignOutButton />
+          <div className={'flex w-full flex-row items-center gap-4'}>
+            <UserButton />
+            <span className={'font-semibold text-gray-600'}>
+              {user.user?.username}
+            </span>
+          </div>
         </SignedIn>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
       </div>
     </div>
   );
