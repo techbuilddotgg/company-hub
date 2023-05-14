af61fb40bf8612732343dfadcf4d739ad6a7101c
gregorsulcer <gregor.sulcer@student.um.si>
2023-05-14 21:42:32 +0200

    fix: home backlink

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
+import { AppRoute } from '@constants/app-routes';
 
 const Logo = () => {
   return (
-    <Link href={'/'}>
+    <Link href={AppRoute.HOME}>
       <a>
         <h1>
           Tech<span className={'text-blue-600'}>Build</span>
