a1b7c4061273f4d785d857b0a3a462083f5b88df
jernej10 <jernej.ofic@gmail.com>
2023-05-15 15:30:25 +0200

    Feat: assign ticket to user

--- src/components/pages/project/task-modal.tsx
+++ src/components/pages/project/task-modal.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Button, DialogContent, Input, Textarea } from '@components';
-import { trpc } from '@utils/trpc';
+import React from "react";
+import { useForm } from "react-hook-form";
+import {
+  Button,
+  Checkbox,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  Input, ScrollArea,
+  Textarea
+} from "@components";
+import { trpc } from "@utils/trpc";
 
 interface FormData {
   name: string;
@@ -25,12 +34,39 @@ export const TaskModal = ({
       description: '',
     },
   });
+
   const { mutate: deleteTaskMutation } = trpc.board.deleteTask.useMutation({
     onSuccess: () => {
       refetch();
       setOpenTaskDialog(false);
     },
   });
+
+  const {data: assignedUsers} = trpc.board.getUsersAssignedToTask.useQuery({ taskId: id});
+  const { mutate: addUserToTask } = trpc.board.addUserToTask.useMutation({
+    onSuccess: () => {
+      //refetch();
+      setOpenTaskDialog(true);
+    },
+  });
+
+  const { mutate: removeUserFromTask } = trpc.board.removeUserFromTask.useMutation({
+    onSuccess: () => {
+      refetch();
+      setOpenTaskDialog(true);
+    },
+  });
+
+  const { data: users, refetch: refetchUsers } = trpc.users.findAll.useQuery();
+
+  const onUserCheckedChange = (userId: string, checked: string |boolean) => {
+    if(checked)
+      addUserToTask({ userId: userId, taskId: id })
+    if(!checked)
+      removeUserFromTask({ userId: userId, taskId: id })
+
+    console.log(userId);
+  }
 
   const onSubmit = (data: FormData) => {
     console.log(data);
@@ -42,6 +78,29 @@ export const TaskModal = ({
 
   return (
     <DialogContent setDialogOpen={setOpenTaskDialog}>
+      <DialogHeader>
+        <DialogTitle>Ticket</DialogTitle>
+        <DialogDescription>
+          Assign user to ticket:
+        </DialogDescription>
+      </DialogHeader>
+      <ScrollArea className="h-44 w-72 rounded-md border">
+        <div className="p-4">
+        {users && users.map((user) => (
+          <div className="items-top flex space-x-2 my-2" key={user.id}>
+            <Checkbox id={`terms${user.id}`} checked={assignedUsers ? assignedUsers.includes(user.id) : false} onCheckedChange={(checked) => onUserCheckedChange(user.id, checked)} />
+            <div className="grid gap-1.5 leading-none">
+              <label
+                htmlFor={`terms${user.id}`}
+                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                {user.emailAddresses[0]?.emailAddress}
+              </label>
+            </div>
+          </div>
+        ))}
+        </div>
+      </ScrollArea>
       <form className={'flex flex-col gap-4'} onSubmit={handleSubmit(onSubmit)}>
         <Input label={'Name'} {...register('name')} />
         <Textarea label={'Description'} {...register('description')} rows={5} />
@@ -51,7 +110,11 @@ export const TaskModal = ({
           </Button>
           <Button type={'submit'}>Update</Button>
         </div>
+
       </form>
     </DialogContent>
+
   );
 };
+
+
