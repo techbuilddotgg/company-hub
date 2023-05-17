05a2eede5243040bb034627005211a0153973398
gregorsulcer <gregor.sulcer@student.um.si>
2023-05-17 11:23:14 +0200

    fix: event-modal layout

--- src/components/pages/calendar/event-modal.tsx
+++ src/components/pages/calendar/event-modal.tsx
@@ -10,10 +10,15 @@ import {
 } from '@components';
 
 const EventModalForm = () => {
+  const [allDay, setAllDay] = React.useState(false);
+
   return (
     <form>
-      <Input label={'Title'} />
-      <div className="my-2 flex items-center space-x-2">
+      <div className={'flex flex-col gap-2'}>
+        <Input label={'Title'} />
+        <DatePicker />
+      </div>
+      <div className="my-2 ml-0.5 flex items-center space-x-2">
         <Checkbox id="all_day" />
         <label
           htmlFor="all_day"
@@ -22,11 +27,12 @@ const EventModalForm = () => {
           All day
         </label>
       </div>
-      <DatePicker />
-      <div className={'my-2 flex gap-2'}>
-        {/*<TimePicker />*/}
-        <TimePicker />
-      </div>
+      {!allDay && (
+        <div className={'my-2 flex gap-2'}>
+          <TimePicker />
+          <TimePicker />
+        </div>
+      )}
       <Textarea label={'Description'} rows={3} />
       <Button className={'ml-auto mt-2'} type={'submit'}>
         Add
