742addac64f2def54a88111c5a7c7bdb96c15f07
gregorsulcer <gregor.sulcer@student.um.si>
2023-05-21 22:24:34 +0200

    feat: user selection search

--- src/components/ui/user-selection.tsx
+++ src/components/ui/user-selection.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import {
   Accordion,
   AccordionContent,
@@ -10,6 +10,8 @@ import {
   ScrollArea,
 } from '@components';
 import { trpc } from '@utils/trpc';
+import { useForm } from 'react-hook-form';
+import { User } from '@clerk/backend';
 
 interface UserSelectionProps {
   selected: string[];
@@ -21,53 +23,80 @@ const UserSelection: FC<UserSelectionProps> = ({
   handleCheckedChange,
 }) => {
   const { data: users } = trpc.users.findAll.useQuery();
+  const [test, setTest] = React.useState<User[] | undefined>([]);
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      search: '',
+    },
+  });
+
+  const search = (searchQuery: string) => {
+    if (searchQuery === '') return setTest(users);
+    const result = users?.filter((user) =>
+      user?.emailAddresses[0]?.emailAddress
+        .toLowerCase()
+        .includes(searchQuery.toLowerCase()),
+    );
+    setTest(result);
+  };
+
+  useEffect(() => {
+    setTest(users);
+  }, [users]);
 
   return (
-    <Accordion type="single" collapsible>
-      <AccordionItem value={'item-2'}>
-        <AccordionTrigger className={'text-base font-semibold'}>
-          Add users
-        </AccordionTrigger>
-        <AccordionContent className={'px-1'}>
-          <Input info={'Search for user'} className={'mb-2'} />
-          <ScrollArea className="h-44 rounded-md border">
-            {users ? (
-              users.map((user, index) => (
-                <>
-                  {user?.emailAddresses[0]?.emailAddress && (
-                    <div className="overflow-auto px-2 py-1" key={index}>
-                      <div className="items-top my-2 flex space-x-2">
-                        <Checkbox
-                          id={user?.emailAddresses[0]?.emailAddress}
-                          checked={selected.includes(
-                            user?.emailAddresses[0]?.emailAddress,
-                          )}
-                          onCheckedChange={(checked) =>
-                            handleCheckedChange(
-                              checked as boolean,
-                              user?.emailAddresses[0]?.emailAddress as string,
-                            )
-                          }
-                        />
-                        <div className="grid gap-1.5 leading-none">
-                          <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                            {user?.emailAddresses[0]?.emailAddress}
-                          </label>
+    <form onChange={handleSubmit((data) => search(data.search))}>
+      <Accordion type="single" collapsible>
+        <AccordionItem value={'item-2'}>
+          <AccordionTrigger className={'text-base font-semibold'}>
+            Add users
+          </AccordionTrigger>
+          <AccordionContent className={'px-1'}>
+            <Input
+              info={'Search for user'}
+              className={'mb-2'}
+              {...register('search')}
+            />
+            <ScrollArea className="h-44 rounded-md border">
+              {test ? (
+                test.map((user, index) => (
+                  <div key={index}>
+                    {user?.emailAddresses[0]?.emailAddress && (
+                      <div className="overflow-auto px-2 py-1">
+                        <div className="items-top my-2 flex space-x-2">
+                          <Checkbox
+                            id={user?.emailAddresses[0]?.emailAddress}
+                            checked={selected.includes(
+                              user?.emailAddresses[0]?.emailAddress,
+                            )}
+                            onCheckedChange={(checked) =>
+                              handleCheckedChange(
+                                checked as boolean,
+                                user?.emailAddresses[0]?.emailAddress as string,
+                              )
+                            }
+                          />
+                          <div className="grid gap-1.5 leading-none">
+                            <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                              {user?.emailAddresses[0]?.emailAddress}
+                            </label>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  )}
-                </>
-              ))
-            ) : (
-              <div className={'flex h-44 items-center justify-center'}>
-                <LoadingSpinner />
-              </div>
-            )}
-          </ScrollArea>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+                    )}
+                  </div>
+                ))
+              ) : (
+                <div className={'flex h-44 items-center justify-center'}>
+                  <LoadingSpinner />
+                </div>
+              )}
+            </ScrollArea>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </form>
   );
 };
 
