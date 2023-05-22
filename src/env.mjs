ff13b780f199c78d727f24e84a4a54667201ef2f
jernej10 <jernej.ofic@gmail.com>
2023-05-22 20:46:29 +0200

    Fix: env

--- src/env.mjs
+++ src/env.mjs
@@ -28,6 +28,12 @@ export const env = createEnv({
     UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
     GITHUB_WEBHOOK_SECRET: z.string().min(1),
     GITHUB_WEBHOOK_LISTENER_URL: z.string().min(1),
+    PUSHER_APP_ID: z.string().min(1),
+    PUSHER_KEY: z.string().min(1),
+    PUSHER_SECRET: z.string().min(1),
+    PUSHER_CLUSTER: z.string().min(1),
+    NEXT_PUBLIC_PUSHER_APP_KEY: z.string().min(1),
+    NEXT_PUBLIC_PUSHER_APP_CLUSTER: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1)
@@ -46,5 +52,11 @@ export const env = createEnv({
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     GITHUB_WEBHOOK_SECRET: process.env.GITHUB_WEBHOOK_SECRET,
     GITHUB_WEBHOOK_LISTENER_URL: process.env.GITHUB_WEBHOOK_LISTENER_URL,
+    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+    PUSHER_KEY: process.env.PUSHER_KEY,
+    PUSHER_SECRET: process.env.PUSHER_SECRET,
+    PUSHER_CLUSTER: process.env.PUSHER_CLUSTER,
+    NEXT_PUBLIC_PUSHER_APP_KEY: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
+    NEXT_PUBLIC_PUSHER_APP_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER,
   }
 });
