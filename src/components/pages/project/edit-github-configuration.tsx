024732fbd232e405185381685d387c212bcee04b
Matevz <semprimoznik.matevz@gmail.com>
2023-05-29 16:48:38 +0200

    fix: refetch board after github integration, add loading to edit github integration modal

--- src/components/pages/project/edit-github-configuration.tsx
+++ src/components/pages/project/edit-github-configuration.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  DataView,
   Label,
   LoaderButton,
   ScrollArea,
@@ -9,10 +10,12 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@components';
-import { trpc } from '@utils/trpc';
+import { RouterOutput, trpc } from '@utils/trpc';
 import { githubEventToText } from '@constants/github-event-to-text';
 import { useQueryClient } from '@tanstack/react-query';
 import { getQueryKey } from '@trpc/react-query';
+
+type GithubDataType = RouterOutput['github']['getWebhookActions'];
 
 interface EditGithubConfigurationProps {
   boardId: string;
@@ -30,13 +33,15 @@ const EditGithubConfiguration = ({
         queryClient.invalidateQueries(
           getQueryKey(trpc.github.getWebhookActions),
         );
+        queryClient.invalidateQueries(getQueryKey(trpc.github.isIntegrated));
         closeDialog();
       },
     });
 
-  const { data: githubData } = trpc.github.getWebhookActions.useQuery({
-    boardId,
-  });
+  const { data: githubData, isLoading: isGithubDataLoading } =
+    trpc.github.getWebhookActions.useQuery({
+      boardId,
+    });
   const { data: projectBoardColumns } = trpc.board.getColumns.useQuery({
     boardId,
   });
@@ -51,47 +56,56 @@ const EditGithubConfiguration = ({
 
   return (
     <div>
-      {githubData?.githubWebhooks.map((action) => (
-        <div key={action.id} className="mt-4">
-          <div>
-            <Label>
-              On {githubEventToText.get(action.actionType)} move task to{' '}
-            </Label>
-            <Select
-              value={action.projectBoardColumnName}
-              onValueChange={(value) =>
-                updateWebhookActionMutation({
-                  id: action.id,
-                  projectBoardColumnName: value,
-                })
-              }
-            >
-              <SelectTrigger className="mt-0.5 w-full">
-                <SelectValue placeholder="Repo" />
-              </SelectTrigger>
-              <SelectContent>
-                <ScrollArea className="h-[200px] w-[350px] rounded-md">
-                  {projectBoardColumns?.map((column) => (
-                    <SelectItem key={column.name} value={column.name}>
-                      {column.name}
-                    </SelectItem>
-                  ))}
-                </ScrollArea>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-      ))}
-      <div className="mt-7 flex flex-row items-center justify-between">
-        <LoaderButton
-          variant="outline"
-          isLoading={isDeleteLoading}
-          onClick={() => deleteGithubIntegration({ boardId })}
-        >
-          Remove integration
-        </LoaderButton>
-        <Button onClick={closeDialog}>Update</Button>
-      </div>
+      <DataView<GithubDataType>
+        isLoading={isGithubDataLoading}
+        data={githubData}
+      >
+        {(githubData) => (
+          <>
+            {githubData.githubWebhooks.map((action) => (
+              <div key={action.id} className="mt-4">
+                <div>
+                  <Label>
+                    On {githubEventToText.get(action.actionType)} move task to{' '}
+                  </Label>
+                  <Select
+                    value={action.projectBoardColumnName}
+                    onValueChange={(value) =>
+                      updateWebhookActionMutation({
+                        id: action.id,
+                        projectBoardColumnName: value,
+                      })
+                    }
+                  >
+                    <SelectTrigger className="mt-0.5 w-full">
+                      <SelectValue placeholder="Repo" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <ScrollArea className="h-[200px] w-[350px] rounded-md">
+                        {projectBoardColumns?.map((column) => (
+                          <SelectItem key={column.name} value={column.name}>
+                            {column.name}
+                          </SelectItem>
+                        ))}
+                      </ScrollArea>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            ))}
+            <div className="mt-7 flex flex-row items-center justify-between">
+              <LoaderButton
+                variant="outline"
+                isLoading={isDeleteLoading}
+                onClick={() => deleteGithubIntegration({ boardId })}
+              >
+                Remove integration
+              </LoaderButton>
+              <Button onClick={closeDialog}>Update</Button>
+            </div>
+          </>
+        )}
+      </DataView>
     </div>
   );
 };
